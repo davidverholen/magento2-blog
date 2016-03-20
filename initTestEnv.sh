edfa188203bf98a8e14d612863cbe4b5f67cf25e
David Verholen <david@verholen.com>
2016-03-20 14:28:44 +0100

    add webserver docker container

--- initTestEnv.sh
+++ initTestEnv.sh
@@ -4,3 +4,4 @@ if [ ! -f composer.phar ]; then
 fi
 /usr/bin/env php composer.phar update --no-interaction --optimize-autoloader
 sudo docker-compose up -d
+mysql -uroot -pmysql -h 127.0.0.1 -P 33306 -e "CREATE DATABASE IF NOT EXISTS magento"
